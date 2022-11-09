d111b9c3a36b05211078f79af4039d7dd1620834
Vasya Tkach <vasatkach8@gmail.com>
2022-11-09 14:25:41 +0300

    add group block

--- src/containers/Landing/Landing.jsx
+++ src/containers/Landing/Landing.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './Landing.module.css';
 import MainFrame from './Components/MainFrame/MainFrame';
+import Groups from './Components/Groups/Groups';
 
 const Landing = () => {
     return (
         <div className={styles.wrapper}>
-            <MainFrame/>
+            <MainFrame />
             <div className={styles.row}></div>
             <div className={styles.row}></div>
-            <div className={styles.row}></div>
+            <Groups />
             <div className={styles.row}></div>
             <div className={styles.row}></div>
         </div>
