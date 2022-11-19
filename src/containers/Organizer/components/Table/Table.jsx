14a0145f5ff278accf1e957708697297c8b649dd
newnoope <artyomstahov33@gmail.com>
2022-11-19 05:22:09 +0200

    CRUD with api, but server error 500

--- src/containers/Organizer/components/Table/Table.jsx
+++ src/containers/Organizer/components/Table/Table.jsx
@@ -18,7 +18,10 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
-// import styles from './Table.module.css';
+import styles from './Table.module.css';
+import TablePagination from '@mui/material/TablePagination';
+import axios from 'axios';
+
 
 const tableIcons = {
 Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -40,70 +43,198 @@ ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
 ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
 
+function PatchedPagination(props: TablePaginationProps) {
+  const {
+    ActionsComponent,
+    onChangePage,
+    onChangeRowsPerPage,
+    ...tablePaginationProps
+  } = props;
+
+  return (
+    <TablePagination
+      {...tablePaginationProps}
+      // @ts-expect-error onChangePage was renamed to onPageChange
+      onPageChange={onChangePage}
+      onRowsPerPageChange={onChangeRowsPerPage}
+      ActionsComponent={(subprops) => {
+        const { onPageChange, ...actionsComponentProps } = subprops;
+        return (
+          // @ts-expect-error ActionsComponent is provided by material-table
+          <ActionsComponent
+            {...actionsComponentProps}
+            onChangePage={onPageChange}
+          />
+        );
+      }}
+    />
+  );
+}
+
+const api = axios.create({
+  baseURL: `http://localhost:8000/api`
+})
 
   function Tablic() {
     const [data, setData] = React.useState([]);
     const [columns, setColumns] = useState([
+      {title: "id", field: "id"},
       { title: "Name", field: "name",},
       { title: "Surname", field: "surname"},
       { title: "Phone", field: "phone_number" },
-      { title: "Acc", field: "status",render: rowData => (rowData.mode ? "Manager" : "Client")
+      { title: "Acc", field: "status",render: rowData => rowData.status ? rowData="Manager" :rowData="Client",
+      lookup:{false:"Client",true:"Manager"}
       },
-      { title: "Tariff", field: "season_ticket_name" }
+      { title: "Tariff", field: "season_ticket_name",render: rowData => rowData.season_ticket_name,
+      lookup:{1:"Basic",2:"Basic+",3:"unlimited"} 
+      }
 
     ]);
 
-  React.useEffect(() => {
-      fetch("http://localhost:8000/api/organizer")
-      .then(resp => resp.json())
-      .then(resp => {
-          setData(resp)
-      })
+    const [iserror, setIserror] = useState(false)
+    const [errorMessages, setErrorMessages] = useState([])
+  React.useEffect(() => { 
+    api.get("/organizer")
+        .then(res => {              
+            setData(res.data)
+         })
+         .catch(error=>{
+             console.log("Error")
+         })
   }, [])
-    return (
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.name === ""){
+      errorList.push("Please enter name")
+    }
+    if(newData.surname === ""){
+      errorList.push("Please enter surname")
+    }
+    if(newData.phone_number === ""){
+      errorList.push("Please enter a valid phone")
+    }
+    if(newData.season_ticket_name === ""){
+      errorList.push("Please enter a valid season ticket")
+    }
+
+    if(errorList.length < 1){
+      api.put("/organizer/"+newData.id, newData)
+      .then(res => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve()
+        setIserror(false)
+        setErrorMessages([])
+      })
+      .catch(error => {
+        setErrorMessages(["Update failed! Server error"])
+        setIserror(true)
+        resolve()
+        
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+
+    }
+    
+  }
+
+  const handleRowAdd = (newData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.name === ""){
+      errorList.push("Please enter name")
+    }
+    if(newData.surname === ""){
+      errorList.push("Please enter surname")
+    }
+    if(newData.phone_number === ""){
+      errorList.push("Please enter a valid phone")
+    }
+    if(newData.season_ticket_name === ""){
+      errorList.push("Please enter a valid season ticket")
+    }
+
+    if(errorList.length < 1){ //no error
+      api.post("/organizer", newData)
+      .then(res => {
+        let dataToAdd = [...data];
+        dataToAdd.push(newData);
+        setData(dataToAdd);
+        resolve()
+        setErrorMessages([])
+        setIserror(false)
+      })
+      .catch(error => {
+        setErrorMessages(["Cannot add data. Server error!"])
+        setIserror(true)
+        resolve()
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+    }
+
+    
+  }
+
+  const handleRowDelete = (oldData, resolve) => {
+    
+    api.delete("/organizer/"+oldData.id)
+      .then(res => {
+        const dataDelete = [...data];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setData([...dataDelete]);
+        resolve()
+      })
+      .catch(error => {
+        setErrorMessages(["Delete failed! Server error"])
+        setIserror(true)
+        resolve()
+      })
+  }
+
+    return (     
       <MaterialTable
       icons={tableIcons}
         title=''
         columns={columns}
         data={data}
-        enableStickyFooter
+        components={{
+          Pagination: PatchedPagination,
+        }}
+        options={{
+          pageSize: 12,
+          pageSizeOptions: [12, 15, 25, 50],
+
+      }}
         editable={{
           
           
-          onRowAdd: (icon:AddBox,newData) =>
-          
-            new Promise((resolve, reject) => {
-              
-              setTimeout(() => {
-                setData([...data, newData]);
+          onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            handleRowAdd(newData, resolve)
+          }),
+            onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+                handleRowUpdate(newData, oldData, resolve);
                 
-                resolve();
-              }, 1000)
             }),
-          onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const dataUpdate = [...data];
-                const index = oldData.tableData.id;
-                dataUpdate[index] = newData;
-                setData([...dataUpdate]);
-  
-                resolve();
-              }, 1000)
-            }),
-          onRowDelete: oldData =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const dataDelete = [...data];
-                const index = oldData.tableData.id;
-                dataDelete.splice(index, 1);
-                setData([...dataDelete]);
-                
-                resolve()
-              }, 1000)
+            onRowDelete: (oldData) =>
+            new Promise((resolve) => {
+              handleRowDelete(oldData, resolve)
             }),
         }}
       />
+      
     )
   }
 export default Tablic
